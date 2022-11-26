b1e522ef49d803b2f6cadd101f8c38c01975acef
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-26 20:40:17 -0300

    Console.log - A primeira coisa que você vai ver em JS

---
+++ console_log/index.js
@@ -0,0 +1,18 @@
+console.log('Texto com aspas simples'); //string
+console.log("Texto com aspas duplas"); //string
+// log é um método que está dentro do objeto console
+// Dentro de console log podemos colocar qualquer coisa
+// Para exibir textos, nós envolvemos entre aspas
+// ponto e vírgula é opcional
+
+console.log(`Texto com crase`) //string
+// Posso utilizar tanto aspas simples quanto aspas duplas, utilizando crase
+
+console.log(12);
+// int number
+
+console.log(1.212312);
+// float number
+
+// Concatenando strings e número
+console.log('na "lua" já são', 10, 'da noite.')
