15aaf4928183f8a6dbbc28da82ba32afd4ae44be
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-27 18:33:49 -0300

    Let vs Var - Primeira diferença

---
+++ let_vs_var/index.js
@@ -0,0 +1,6 @@
+// ECMAScript padronização da linguagem javascript criada pela empresa ECMA international
+var nome = 'nome';
+var nome = 'new name';
+// Posso sobrescrever a variável com var diferente de let
+
+nome = 'Outro nome' // NÃO FAÇA ISSO, PODE GERAR ERROS NO SISTEMA
