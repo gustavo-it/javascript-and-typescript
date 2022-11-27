59815f8dfe65a74dc6b8a7b363dcbb1745cbb0de
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-27 10:30:22 -0300

    Navegador vs Node (HTML + JavaScript)

---
+++ navegador_vs_node/js/index.js
@@ -0,0 +1,11 @@
+console.log('ola navegador')
+/* 
+O console.log só é exibido no console do navegador. NÃO
+É EXIBIDO NA PÁGINA HTML
+*/
+alert('uma string qualquer!!')
+// Exibindo um alerta na página
+/* 
+O node js não consegue executar um alert. Esse alert só é
+executado no navegador.
+*/
