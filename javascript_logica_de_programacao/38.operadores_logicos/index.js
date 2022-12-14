0c264e9a601d8b16aaca717c83684a2fa04b73e1
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-14 14:41:54 -0300

    Operadores lógicos

---
+++ javascript_logica_de_programacao/38.operadores_logicos/index.js
@@ -0,0 +1,11 @@
+/* 
+Operadores Lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+console.log(true && true); // retorna true
+console.log(false || false || true || false); // retorna true
+console.log(false || false || false || false); // retorna false
+console.log(!false); // inverte uma expressão
+console.log(!!false); // podemos negar duas vezes a expressão
