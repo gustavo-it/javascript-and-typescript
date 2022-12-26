e56495065294a020b66236ea9e6803f08b43dd86
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-26 15:10:27 -0300

    For of - Estrutura de repetição

---
+++ javascript_logica_de_programacao/56.for_of/index.js
@@ -0,0 +1,10 @@
+// For of - Estrutura de repetição
+
+const nome = 'Maria fernanda';
+
+// Desta maneira ele não me retorna o indice
+for (let valor of nome) {
+    console.log(valor);
+}
+
+// Não podemos utilizar esse 'for' em um objeto
