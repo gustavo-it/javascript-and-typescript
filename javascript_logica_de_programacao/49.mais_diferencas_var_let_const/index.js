4f2185bf5d223663d9d47e44b5d59f86d355cbe6
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-26 11:07:14 -0300

    Mais diferenças entre var e let/const

---
+++ javascript_logica_de_programacao/49.mais_diferencas_var_let_const/index.js
@@ -0,0 +1,11 @@
+// Não podemos redeclarar let.
+// posso redeclarar var.
+
+// com var o escopo é global(função).
+// let tem escopo de bloco.
+
+// let vai buscando o valor bloco, por bloco.
+// se ele não encontrar dentro do 'if', por exemplo, vai procurar fora do bloco.
+
+// var é um escopo GLOBAL. Você pode redeclarar um var em uma função.
+// o valor que você declarou na função, será o novo valor de var.
