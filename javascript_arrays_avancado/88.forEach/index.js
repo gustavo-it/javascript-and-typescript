5914ad982898da3c1e4f5d2af72136c143237698
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-01-02 11:27:59 -0300

    forEach

---
+++ javascript_arrays_avancado/88.forEach/index.js
@@ -0,0 +1,8 @@
+// forEach
+
+// forEach só esta disponível dentro de arrays
+// forEach só vai iterar sobre os valores
+// Recebe uma função
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numeros.forEach(valor => console.log(valor + 1)); // iterando sobre array
